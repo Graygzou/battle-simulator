f40d59615a00c839d05b2139f53f4577c074140f
theo-le-donne <theo.ldonne@gmail.com>
2018-04-01 18:21:48 -0400

    crawler done

--- Exercice1/src/main/scala/Crawler.scala
+++ Exercice1/src/main/scala/Crawler.scala
@@ -1,55 +1,66 @@
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 object Crawler extends App{
 
-  var bestiaries = new Array[String](5)
-  bestiaries(0) = "http://paizo.com/pathfinderRPG/prd/bestiary/monsterIndex.html"
-  bestiaries(1) = "http://paizo.com/pathfinderRPG/prd/bestiary2/additionalMonsterIndex.html"
-  bestiaries(2) = "http://paizo.com/pathfinderRPG/prd/bestiary3/monsterIndex.html"
-  bestiaries(3) = "http://paizo.com/pathfinderRPG/prd/bestiary4/monsterIndex.html"
-  bestiaries(4) = "http://paizo.com/pathfinderRPG/prd/bestiary5/index.html"
+  var indexes = new Array[String](5)
+  //val baseUrl = "http://paizo.com/pathfinderRPG/prd/bestiary"
+  indexes(0) = "http://paizo.com/pathfinderRPG/prd/bestiary/monsterIndex.html"
+  indexes(1) = "http://paizo.com/pathfinderRPG/prd/bestiary2/additionalMonsterIndex.html"
+  indexes(2) = "http://paizo.com/pathfinderRPG/prd/bestiary3/monsterIndex.html"
+  indexes(3) = "http://paizo.com/pathfinderRPG/prd/bestiary4/monsterIndex.html"
+  indexes(4) = "http://paizo.com/pathfinderRPG/prd/bestiary5/index.html"
+  val creatures = new ArrayBuffer[Creature]
 
-  crawlIndex(bestiaries(0))
+  //crawlIndex(4)
 
-  def crawlIndex(url : String) : Unit = {
-    val index = Source.fromURL(url).mkString
+  for (i <- 0 to 4) {
+    crawlIndex(i)
+    println(creatures.toString())
+
+  }
+
+  def crawlIndex(indexNb : Int) : Unit = {
+    val index = Source.fromURL(indexes(indexNb)).mkString
     val indexReduced = index.substring(index.indexOf("""id="monster-index-wrapper""""),index.indexOf("footer"))
     val monsterLinks = Source.fromString(indexReduced).getLines().filter(line => line.contains("a href"))
-    //crawlSpell("http://paizo.com/pathfinderRPG/prd/bestiary/choker.html#choker","choker")
     while (monsterLinks.hasNext) {
       val link = monsterLinks.next()
       if (link.contains("#")) {
-        val monsterType = link.substring(link.indexOf("a href=") + 8, link.indexOf(".html"))
+        //val monsterType = link.substring(link.indexOf("a href") + 8, link.indexOf(".html"))
         val monsterName = link.substring(link.indexOf(".html") + 6, link.indexOf("""">"""))
-        val url = "http://paizo.com/pathfinderRPG/prd/bestiary/" + link.substring(link.indexOf("a href=") + 8, link.indexOf("""">"""))
-        println("Type : " + monsterType + " Name : " + monsterName)
-        crawlSpell(url,monsterName)
-
+        var monsterUrl = "http://paizo.com" + link.substring(link.indexOf("a href") + 8, link.indexOf("""">""")).replace(" ","").replace(""""""","")
+        if (indexNb == 0) {
+          monsterUrl = "http://paizo.com/pathfinderRPG/prd/bestiary/" + link.substring(link.indexOf("a href=") + 8, link.indexOf("""">"""))
+        }
+        crawlSpell(monsterUrl,monsterName, indexNb)
       }
     }
   }
 
 
-  def crawlSpell(url : String, monsterName : String): Unit = {
+  def crawlSpell(url : String, monsterName : String, indexNb : Int): Unit = {
     val source = Source.fromURL(url).mkString
     val monsterBody = source.substring(source.indexOf("body-content")+16,source.indexOf("footer"))
     val monsters = monsterBody.split("<h1 ")
     for (i <- 1 until monsters.length) {
       if (monsterName.equals(monsters(i).split(""""""")(1))) {
-        val creature = new Creature(monsterName)
-        if (monsters(i).contains("Special Abilities") && !monsterName.contains("dragon")) {
-          //println(monsters(i).substring(monsters(i).indexOf("Special Abilities")))
+        val creature = new Creature(monsterName.replace(",","").replace("-"," "))
+        if (monsters(i).contains("Special Abilities") && !monsterName.contains("dragon"))
           monsters(i)=monsters(i).substring(0,monsters(i).indexOf("Special Abilities"))
-        }
-        val links = Source.fromString(monsters(i)).getLines().filter(line => line.contains("spells") && line.contains("href")).mkString.split("<a href=")
+        val links = Source.fromString(monsters(i)).getLines().filter(line => line.contains("spells") && line.contains("href")).mkString.split("a href")
         for (link <- links) {
-          //println(link)
           if (link.contains("spells") && link.contains("html")) {
-            creature.addspell(link.substring(link.indexOf(">")+1,link.indexOf("<")))
+            if (link.contains("fly.html"))
+              creature.addspell("fly")
+            else
+              creature.addspell(link.substring(link.indexOf(".html#")+6,link.indexOf(""">""")).replace(""""""","").replace(" ","").replace("-s-","'s ").replace("-"," "))
           }
         }
-
-        println(creature.toString)
+        if (!creatures.contains(creature.name) && creature.spells.nonEmpty) {
+          println(creature.toString)
+          creatures.append(creature)
+        }
       }
     }
   }
--- Exercice1/src/main/scala/Creature.scala
+++ Exercice1/src/main/scala/Creature.scala
@@ -1,10 +1,18 @@
 import scala.collection.mutable.ArrayBuffer
 
-class Creature(val name : String) extends Serializable {
+class Creature(val creatureName : String) extends Serializable {
   var spells =  ArrayBuffer[String]()
+  val name = creatureName
   def addspell(spell : String) : Unit = {
     if (!spells.contains(spell))
       spells += spell
+  }
+
+  def isEmpty(): Boolean = {
+    if (spells.isEmpty)
+      return true
+    else
+      return false
   }
 
   override def toString() : String = {
