afeacb8cb20a725594d0eaa827127cbe7da8695b
Graygzou <gregoire.boiron@gmail.com>
2018-04-03 12:06:03 -0400

    Create basic functions of entity class.

--- Exercice2/battlesimulator/src/main/scala/com/graygzou/Entity.scala
+++ Exercice2/battlesimulator/src/main/scala/com/graygzou/Entity.scala
@@ -16,4 +16,25 @@ class Entity(name: String, health: Double, armor : Double, strength: Double, reg
   def getStrength = ownStrength
   def getRegeneration = ownRegeneration
 
+  // Functions that will be used for the simulation
+
+  // Attack
+  def Attack(target:Entity, damages: Double) =
+    println(s"I'm attacking ${target.toString} with the current damages $damages")
+
+  // Move
+  def Move(x: Double, y: Double, z: Double) =
+    println(s"I'm moving to ($x $y $z) position")
+
+  // TODO : right the correct body of this function
+  override def toString: String = super.toString
+
+  // ----- Others ------
+
+  // Heal
+
+  // Buff
+
+  // Flying => interfaces ?
+
 }
--- Exercice2/battlesimulator/src/main/scala/com/graygzou/TestEntity.scala
+++ Exercice2/battlesimulator/src/main/scala/com/graygzou/TestEntity.scala
@@ -1,15 +1,27 @@
 package com.graygzou
 
-/**
- * Hello world!
- *
- */
-
 object TestEntity {
   def main(args: Array[String]){
     //println( "Hello World!" )
 
     // Test the entity class
-    
+
+    // Create an orc
+    val monster1 = new Entity("Orc", 1D, 2D, 3D, 4D)
+    printf("Name of the monster %s\n", monster1.getName)
+    printf("Health %f\n", monster1.getHealth)
+    printf("Armor %f\n", monster1.getArmor)
+    printf("Strength %f\n", monster1.getStrength)
+    printf("Regeneration %f\n", monster1.getRegeneration)
+    print("\n")
+
+    // Create a dragon
+    val monster2 = new Entity("Dragon", 100D, 200D, 300D, 400D)
+    printf("Name of the monster %s\n", monster2.getName)
+    printf("Health %f\n", monster2.getHealth)
+    printf("Armor %f\n", monster2.getArmor)
+    printf("Strength %f\n", monster2.getStrength)
+    printf("Regeneration %f\n", monster2.getRegeneration)
+
   }
 }
