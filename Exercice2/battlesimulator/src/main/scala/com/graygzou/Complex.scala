8f248592e5b335d1f89b028e0c14d43a265069aa
Graygzou <gregoire.boiron@gmail.com>
2018-04-11 19:06:58 -0400

    Add examples, clean the project, review a bunch of stuff.

--- Exercice2/battlesimulator/src/main/scala/com/graygzou/Complex.scala
+++
@@ -1,8 +0,0 @@
-package com.graygzou
-
-class Complex(real: Double, imaginary: Double) {
-  def re = real
-  def im = imaginary
-  override def toString() =
-    "" + re + (if (im < 0) "" else "+") + im + "i"
-}
---
+++ Exercice2/battlesimulator/src/main/scala/com/graygzou/Examples/Complex.scala
@@ -0,0 +1,8 @@
+package com.graygzou.Examples
+
+class Complex(real: Double, imaginary: Double) {
+  def re = real
+  def im = imaginary
+  override def toString() =
+    "" + re + (if (im < 0) "" else "+") + im + "i"
+}
